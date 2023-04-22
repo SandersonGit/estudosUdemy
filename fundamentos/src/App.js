703995c850e3554cdbaa3fb0428179a2812acc8d
Sanderson Luiz Celestino <sandersonluiz1988@gmail.com>
2023-04-21 21:06:40 -0300

    Fundamentos React

---
+++ fundamentos/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import FristComponent from './components/FristComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>FUNDAMENTOS</h1>
+      <FristComponent/>
+    </div>
+  );
+}
+
+export default App;
