6a5e30bfb0a2752932f774240e14f217767bad84
Sanderson Luiz Celestino <sandersonluiz1988@gmail.com>
2023-04-22 22:25:18 -0300

    Conceito de props e desestruturação de valores

---
+++ 1_fundamento/src/components/CarDetails.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const CarDetails = ({ marca, km, cor }) => {
+  return (
+    <div>
+        <h2>Detalhes do carro</h2>
+      <ul>
+        <li>Marca: {marca}</li>
+        <li>KM: {km} </li>
+        <li>Cor: {cor} </li>
+      </ul>
+    </div>
+  );
+};
+
+export default CarDetails;
---
+++ 1_fundamento/src/components/CasaDetail.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const CasaDetail = ({nome, metragem, valor}) => {
+  return (
+    <div>
+        <h2>Propriedade da casa</h2>
+        <ul>
+            <li>Nome: {nome} </li>
+            <li>Metragem: {metragem}</li>
+            <li>Valor: {valor}</li>
+        </ul>
+    </div>
+  )
+}
+
+export default CasaDetail
