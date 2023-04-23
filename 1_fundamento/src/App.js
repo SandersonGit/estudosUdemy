6f425ffd59aff329a18b9021b4afb159e7e0467e
Sanderson Luiz Celestino <sandersonluiz1988@gmail.com>
2023-04-22 22:57:33 -0300

    Conceito de props reutilização de componentes

--- 1_fundamento/src/App.js
+++ 1_fundamento/src/App.js
@@ -1,6 +1,5 @@
-
 import "./App.css";
-import Ash from "./assets/1150.png"
+import Ash from "./assets/1150.png";
 import CarDetails from "./components/CarDetails";
 import CasaDetail from "./components/CasaDetail";
 import CondicionalRender from "./components/CondicionalRender";
@@ -9,12 +8,9 @@ import ManageData from "./components/ManageData";
 import UserName from "./components/UserName";
 
 function App() {
-
-const casaNome = "Augusta"
-const casaMetragem = "100m"
-const casaValor = 100.000 
-   
-
+  const casaNome = "Augusta";
+  const casaMetragem = "100m";
+  const casaValor = 100.0;
 
   return (
     <div className="App">
@@ -27,25 +23,20 @@ const casaValor = 100.000
       <p>Imagen importada da pasta public</p>
       <img src="/picachu1.png" alt="Picachu" />
       {/* carregar pela pasta assets em src */}
-
       <div>
         <p>Imagem importada da pasta assets em src</p>
-      <img src={Ash} alt="Ash" />
+        <img src={Ash} alt="Ash" />
       </div>
-
       <h1>UseState</h1>
-      <ManageData/>
-
-      <ListRender/>
-
-      <CondicionalRender/>
-
-      <UserName name= "Sanderson" age= "35"/>
-
-      <CarDetails marca="VW" km={10.000} cor= "Branco" />
-
-      <CasaDetail nome= {casaNome} metragem ={casaMetragem} valor={casaValor}/>
-
+      <ManageData />
+      <ListRender />
+      <CondicionalRender />
+      <UserName name="Sanderson" age="35" />
+      <CarDetails marca="VW" km={10.0} cor="Branco" newCar={false} />
+      <CarDetails marca="Ford" km={3.0} cor="Azul" newCar={false} />
+      <CarDetails marca="Peugeot" km={0} cor="Vermelho" newCar={true} />
+      <CarDetails marca="Toyota" km={45.0} cor="Branco" newCar={false} />
+      <CasaDetail nome={casaNome} metragem={casaMetragem} valor={casaValor} />
     </div>
   );
 }
