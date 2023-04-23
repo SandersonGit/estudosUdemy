87bfd321ae30560022631690d1af40b437cb3b12
Sanderson Luiz Celestino <sandersonluiz1988@gmail.com>
2023-04-23 00:12:03 -0300

    passando funçoes por props de pai para filho

---
+++ 1_fundamento/src/components/ExecuteFunction.jsx
@@ -0,0 +1,10 @@
+
+const ExecuteFunction = ({mostrarNome}) => {
+  return (
+    <div>
+        <button onClick={mostrarNome} >Clique em mim</button>
+    </div>
+  )
+}
+
+export default ExecuteFunction
